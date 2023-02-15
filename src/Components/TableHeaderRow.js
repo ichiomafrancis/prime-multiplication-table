5e6b499245077cef06b4fd7fb448f0ca090a0d77
Francis Chioma <ichiomafrancis@gmail.com>
2023-02-15 17:55:08 +0100

    made some changes to the mapping

--- src/Components/TableHeaderRow.js
+++ src/Components/TableHeaderRow.js
@@ -1,18 +1,17 @@
 import React from "react";
 
-const TableHeaderRow = (primeNos) => {
-  // const primeNos = [2, 3, 5, 7, 11, 13, 17];
-  const headerRow = primeNos.map((num) => {
-    return `<th>${num}</th>`;
+const TableHeaderRow = () => {
+  const primeNos = [2, 3, 5, 7, 11, 13, 17];
+  const headerRow = primeNos.map((num, i) => {
+    return <th key={i}>{num}</th>;
   });
+  // console.log(tableData);
 
   return (
-    <div>
-      <tr>
-        <th>{""}</th>
-        {headerRow}
-      </tr>
-    </div>
+    <tr>
+      <th>{""}</th>
+      {headerRow}
+    </tr>
   );
 };
 
